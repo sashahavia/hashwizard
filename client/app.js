8c65e97cccbfefd390b7028997cb58952063aa5c
Alexandra Havia <sasha.havia@gmail.com>
2018-08-01 20:20:18 -0400

    Feature (user): User can upload an image

--- client/app.js
+++ client/app.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import FileUpload from './components/fileupload'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <FileUpload />
     </div>
   )
 }
